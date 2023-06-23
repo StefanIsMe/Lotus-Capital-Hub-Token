f9664e6e8d2aa07b1be947fb49f777f32757ef15
Stefan Carter <130151819+StefanIsMe@users.noreply.github.com>
2023-06-23 19:38:33 +0700

    Update Polygon zkEVM.sol

--- contract/Polygon zkEVM/Polygon zkEVM.sol
+++ contract/Polygon zkEVM/Polygon zkEVM.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT pragma solidity ^0.8.0;
 
-import “@openzeppelin/contracts/token/ERC20/ERC20.sol”; import “@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol”; // Imported ERC20Votes for voting and snapshots import “@openzeppelin/contracts/security/ReentrancyGuard.sol”; import “@openzeppelin/contracts/access/Ownable.sol”; import “@openzeppelin/contracts/security/Pausable.sol”;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract PolygonWrapperToken is ERC20, ERC20Votes, ReentrancyGuard, Ownable, Pausable { // Inherited from ERC20Votes uint256 public gasLimit; address public immutable bnbTokenAddress;
 
